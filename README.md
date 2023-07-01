e8df251a1feec5e4c3d22602d773f32e350ed0c8
Roy Montano <roymontano166@gmail.com>
2023-06-30 23:19:31 -0500

    Primer deploy a Railway

--- README.md
+++ README.md
@@ -19,6 +19,20 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la app en dev:
+```
+yarn start:dev
+```
+
+8. Recontruir la base de datos con la semilla
+```
+https://localhost:3000/api/v2/seed
+```
+
 # Stack usado
 * MongoDB
 * Nest
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,16 +5,22 @@ import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon.module';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
-
+  private defaultLimit: number;
   
   constructor (
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService,
+  ) {
+    this.defaultLimit = configService.get<number>('defaultLimit');
+  }
 
 
 
@@ -33,8 +39,17 @@ export class PokemonService {
 
 
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+    
+    const {limit = this.defaultLimit, offset = 0} = paginationDto;
+
+    return this.pokemonModel.find()
+    .limit(limit)
+    .skip(offset)
+    .sort({
+      no: 1
+    })
+    .select('-__v');
   }
 
   async findOne(term: string) {
